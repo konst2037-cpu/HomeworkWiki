18ffce2985801d05c82e8274f8a67cf87162f602
gugupy <gugu.ap900@gmail.com>
2025-09-06 18:46:55 +0530

    prompt from env

--- api/service.py
+++ api/service.py
@@ -1,12 +1,15 @@
 from openai import OpenAI
 from pydantic import BaseModel
 from api.models import GPTStatus
+import os
 
 
 prompt = """You are managing a public record of school homework. Pupils are submitting homework records for school subjects. Your task is to analyse the proposed records and decide if they should be published in the public record. The homework is: “XXX”. The subject is “YYY”. Here are some criteria: 
 It should be plausible that the record is a homework for the given subject. 
 The suggested record must not use strong language and must not be insulting to anyone. 
 The suggested record must not mention attributes or give names to any person. """
+
+prompt = os.getenv("CONTENT_FILTER_PROMPT", prompt)
 
 
 class ContentResponse(BaseModel):
